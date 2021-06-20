66016306327ffad2acebe009402942c793326c1a
Chen Keinan <hen.keinan@gmail.com>
2021-06-20 23:29:26 +0300

    fix: remove testify dependency

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/chen-keinan/go-simple-config
 
 go 1.16
 
-require (
-	github.com/stretchr/testify v1.7.0
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
-)
+require gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
--- simple/config_test.go
+++ simple/config_test.go
@@ -1,82 +1,124 @@
 package simple
 
 import (
-	"github.com/stretchr/testify/assert"
+	"fmt"
 	"os"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
 	c := New()
-	assert.Equal(t, len(c.config), 0)
+	if l := len(c.config); l != 0 {
+		t.Fatal(fmt.Sprintf("%d not equal to %d", l, 0))
+	}
 }
 
 func TestConfig_GetValueString(t *testing.T) {
 	c := New()
 	err := c.Load("./fixture/config.default.json")
-	assert.NoError(t, err)
-	assert.Equal(t, c.GetStringValue("SERVER.host"), "127.0.0.1")
-	assert.Equal(t, c.GetStringValue("PARAMS.retention"), "4")
-	assert.Equal(t, c.GetStringValue("PARAMS.test"), "true")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if value := c.GetStringValue("SERVER.host"); value != "127.0.0.1" {
+		t.Fatal(fmt.Sprintf("%s not equal to %s", value, "127.0.0.1"))
+	}
+	if value := c.GetStringValue("PARAMS.retention"); value != "4" {
+		t.Fatal(fmt.Sprintf("%s not equal to %s", value, "4"))
+	}
+	if value := c.GetStringValue("PARAMS.test"); value != "true" {
+		t.Fatal(fmt.Sprintf("%s not equal to %s", value, "true"))
+	}
 }
 
 func TestConfig_LoadJson(t *testing.T) {
 	c := New()
 	err := c.Load("./fixture/config.default.json")
-	assert.NoError(t, err)
-	assert.Equal(t, c.GetStringValue("SERVER.host"), "127.0.0.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if value := c.GetStringValue("SERVER.host"); value != "127.0.0.1" {
+		t.Fatal(fmt.Sprintf("%s not equal to %s", value, "127.0.0.1"))
+	}
 }
 
 func TestConfig_LoadJsonError(t *testing.T) {
 	c := New()
 	err := c.Load("./fixture/config.default1.json")
-	assert.Error(t, err)
+	if err == nil {
+		t.Fatal(err)
+	}
 }
 
 func TestConfig_LoadJsonBadJson(t *testing.T) {
 	c := New()
 	err := c.Load("./fixture/bad.config.default.json")
-	assert.Error(t, err)
+	if err == nil {
+		t.Fatal(err)
+	}
 }
 
 func TestConfig_LoadJsonBadYaml(t *testing.T) {
 	c := New()
 	err := c.Load("./fixture/bad.config.default.yaml")
-	assert.Error(t, err)
+	if err == nil {
+		t.Fatal(err)
+	}
 }
 
 func TestConfig_LoadYaml(t *testing.T) {
 	c := New()
 	err := c.Load("./fixture/config.default.yaml")
-	assert.NoError(t, err)
-	assert.Equal(t, c.GetStringValue("SERVER.host"), "127.0.0.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if value := c.GetStringValue("SERVER.host"); value != "127.0.0.1" {
+		t.Fatal(fmt.Sprintf("%s not equal to %s", value, "127.0.0.1"))
+	}
 }
 func TestConfig_LoadYml(t *testing.T) {
 	c := New()
 	err := c.Load("./fixture/config.default.yaml")
-	assert.NoError(t, err)
-	assert.Equal(t, c.GetStringValue("SERVER.host"), "127.0.0.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if value := c.GetStringValue("SERVER.host"); value != "127.0.0.1" {
+		t.Fatal(fmt.Sprintf("%s not equal to %s", value, "127.0.0.1"))
+	}
 }
 
 func TestConfig_LoadProperties(t *testing.T) {
 	c := New()
 	err := c.Load("./fixture/config.default.properties")
-	assert.NoError(t, err)
-	assert.Equal(t, c.GetStringValue("SERVER.host"), "127.0.0.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if value := c.GetStringValue("SERVER.host"); value != "127.0.0.1" {
+		t.Fatal(fmt.Sprintf("%s not equal to %s", value, "127.0.0.1"))
+	}
 }
 
 func TestConfig_LoadIni(t *testing.T) {
 	c := New()
 	err := c.Load("./fixture/config.default.ini")
-	assert.NoError(t, err)
-	assert.Equal(t, c.GetStringValue("SERVER.host"), "127.0.0.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if value := c.GetStringValue("SERVER.host"); value != "127.0.0.1" {
+		t.Fatal(fmt.Sprintf("%s not equal to %s", value, "127.0.0.1"))
+	}
 }
 
 func TestConfig_LoadEnv(t *testing.T) {
 	err := os.Setenv("SERVER.host", "127.0.0.1")
-	assert.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	c := New()
 	err = c.Load()
-	assert.NoError(t, err)
-	assert.Equal(t, c.GetStringValue("SERVER.host"), "127.0.0.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if value := c.GetStringValue("SERVER.host"); value != "127.0.0.1" {
+		t.Fatal(fmt.Sprintf("%s not equal to %s", value, "127.0.0.1"))
+	}
 }
